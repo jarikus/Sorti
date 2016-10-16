7979a663a4943b5dd42d4e21eae0ea2eae6d625f
jarikus <jarikus@list.ru>
2016-10-16 17:57:06 +0300

    Заполнение массива случайно сгенерированным размером массива и случайно сгенерированными значениями элементов этого массива.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
 
-public class Main {
+import java.util.Random;
 
+public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Random rand = new Random(); 
+		int n = rand.nextInt(30); //Генерируем размер массива в пределе 30 значений
+		System.out.println ("Размер массива " + n); 
+
+		int[] array = new int[n];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = rand.nextInt(n); //Заполняем массив
+			System.out.println(array[i]);
+		}
 
 	}
 
